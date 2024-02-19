735795b0bff16109e693e949d210ad8ffe5e6452
ΛTᏥΛᏒᐯΛ <115201572+atharv0905@users.noreply.github.com>
2024-02-19 11:12:01 +0530

    Go To Current Location
    Fetching current location of user and setting marker to it's current location

--- app/src/main/java/com/atharv/potholedetector/MapsActivity.java
+++ app/src/main/java/com/atharv/potholedetector/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.atharv.potholedetector;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,42 +12,61 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.atharv.potholedetector.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
-private ActivityMapsBinding binding;
+    Button currentLocationBtn;
 
+    // --------------------------------------------------------------------------------------------------------------------------
+    // setting marking to current location
+    private void goToCurrentLocation() {
+        GPSTracker gpsTracker = new GPSTracker(MapsActivity.this);
+        if (gpsTracker.canGetLocation()) {
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+
+            // creating latlng object
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            // creating marker object
+            MarkerOptions marker = new MarkerOptions().position(latLng).title("My Current Location");
+
+            // adding marker to map
+            mMap.addMarker(marker);
+
+            // moving camera to desired latlng
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            // zoom camera to desired latlng
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f));
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+    }
+    // setting marking to current location
+    // --------------------------------------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-     binding = ActivityMapsBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        // finding map by fragment id
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.gmap);
         mapFragment.getMapAsync(this);
+
+        currentLocationBtn = (Button) findViewById(R.id.currentLocation);
+        currentLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCurrentLocation();
+            }
+        });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
+    @Override // all map related functionalities will be done here
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
